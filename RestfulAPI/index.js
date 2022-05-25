92b4f3084e2959202b2e2fd008bc899d1e838f6f
Rijo do q'até t'apagas <guilherme.belchior1@hotmail.com>
2022-05-25 19:11:40 +0100

    ended API part

--- RestfulAPI/index.js
+++ RestfulAPI/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi'); //Joi 13.1.0
 const express = require('express');
 const app = express();
 
@@ -18,18 +19,75 @@ app.get('/api/words', (req, res) => {
 
 app.get('/api/words/:id', (req, res) => {
     const word1 = words.find( c => c.id === parseInt(req.params.id));
-    if (!word1) res.status(404).send('There\'s no number with such id');
+    if (!word1) return res.status(404).send('There\'s no number with such id');
     else res.send(word1);
 });
 
+
+
+
+
+function validateWord(word) {
+    const schema = {
+        word: Joi.string().min(3).required(),                               
+    };
+
+    return Joi.validate(word, schema);
+}
+
+
+
 app.post('/api/words', (req, res) => {
-    const word1 = {
-        id: words.length +1, //Given by database, mas como não sei usar isso vai assim mm
-        name: req.body.name
+
+    const {error} = validateWord(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message)
+        return;
     }
-    words.push(word1);
+
+    const word1 = {
+        id: words.length +1, //Given by database, mas como não sei usar isso vai assim mm (id = tamanho+1)
+        word: req.body.word  //Verificar com POSTMAN
+    }
+    words.push(word1);      //Adiciona a palavra à array
     res.send(word1);
 });
+
+
+app.put('/api/words/:id', (req, res) =>  {
+    //Searchs the word
+    const word1 = words.find( c => c.id === parseInt(req.params.id));  
+    
+    //If it does not exist, return 404 error     
+    if (!word1) return res.status(404).send('There\'s no number with such id');   
+
+    //Validate
+    const {error}  = validateWord(req.body);
+    //If invalid return 400 - Bad request
+    if (error) {
+        res.status(400).send(error.details[0].message)               
+        return;
+    }
+
+    //Update words
+    word1.word = req.body.word;
+    res.send(word1);
+    //Return the updated words
+});
+
+app.delete('/api/words/:id', (req, res) => {
+    //Look up the code
+    //Not existing, return 404
+    const word1 = words.find( c => c.id === parseInt(req.params.id));
+    if (!word1) return res.status(404).send('There\'s no number with such id');
+
+    //delete
+    const index = words.indexOf(word1);
+    words.splice(index, 1);
+    //Return the same word
+    res.send(word1);
+})
 
 const port = process.env.PORT || 3000;
 
